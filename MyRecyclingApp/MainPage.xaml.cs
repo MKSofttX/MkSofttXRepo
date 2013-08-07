3fc716d90e22a9f91dc8801b585f6f7556577b13
Mksofttx <mksofttx@outlook.com>
2013-08-06 21:13:44 -0500

    Primera version del proyeto

---
+++ MyRecyclingApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using MyRecyclingApp.Resources;
+
+namespace MyRecyclingApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Loaded += MainPage_Loaded;
+        }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //throw new NotImplementedException();
+
+        }
+
+    }
+}
